a1bb80f49147a2c3ebf8ea7c41e0d75109d39fac
khoind <khoind@blockchainlabs.asia>
2018-08-15 15:41:37 +0700

    update .next

--- .next/server/bundles/pages/_error.js
+++ .next/server/bundles/pages/_error.js
@@ -67,28 +67,28 @@ module.exports =
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 21);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 21:
+/***/ "./node_modules/next/dist/pages/_error.js":
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(22);
+module.exports = __webpack_require__("next/error")
 
 
 /***/ }),
 
-/***/ 22:
+/***/ 1:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(23)
+module.exports = __webpack_require__("./node_modules/next/dist/pages/_error.js");
 
 
 /***/ }),
 
-/***/ 23:
+/***/ "next/error":
 /***/ (function(module, exports) {
 
 module.exports = require("next/error");
